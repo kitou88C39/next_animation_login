89b8aca11e0e4e26f0a20af1cf6c0a80ab556d96
kitou88C39 <kanfutrooper@icloud.com>
2023-02-14 20:41:20 +0900

    api/Login.js アニメーション作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 6s linear infinite',
+      },
+    },
   },
   plugins: [],
 };
