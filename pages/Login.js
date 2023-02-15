5e08a59de5aa3afb4d889eef418a2dbcef35a404
kitou88C39 <kanfutrooper@icloud.com>
2023-02-15 20:39:17 +0900

    api/Login.js useForm 作成

--- pages/Login.js
+++ pages/Login.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 export default function Login() {
+  const {
+    handleSubmit,
+    register,
+    formState: { error },
+  } = useForm();
   return (
     <div className='flex items-center justify-center w-full min-h-screen bg-gray-900'>
       <div className='relative w-[380px] h-[420px] bg-gray-100 rounded-lg overflow-hidden'>
         <div className='origin-bottom-right abusolute w-[380px] h-[420px] bg-gradient-to-r from-lime-400 via-lime-400 to-transparent -top-[50%] -left-[50%] animate-spin-slow'></div>
         <div className='origin-bottom-right abusolute w-[380px] h-[420px] bg-gradient-to-r from-lime-400 via-lime-400 to-transparent -top-[50%] -left-[50%] animate-spin-delay'></div>
         <div className='absolute z-10 p-5 bg-gray-600 rounded-lg inset-1'>
-          <form>
+          <form onSubmit={handleSubmit}>
             <h2 className='mb-12 text-xl font-semibold text-center text-lime-400'>
               Sign In
             </h2>
@@ -18,6 +24,12 @@ export default function Login() {
                 autoFocus
                 placeholder=''
                 className='relative z-10 h-10 px-2 text-gray-100 bg-transparent border-0 border-b-2 outline-none border-lime-400 peer'
+                {...register('email'), {
+                  required:"please enter email",
+                  pattern:{
+                    message:"Please enter vaild email" 
+                  }
+                }}
               />
               <i
                 className='absolute bottom-0 left-0 w-full h-10 duration-500 origin-bottom transform rounded bg-lime-400 -z-10
